c2fcc68c18872f648708b8e462d2f995b8957962
JaredRichardson92 <51789426+JaredRichardson92@users.noreply.github.com>
2023-09-12 19:09:35 +0000

    Completed 03_01

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -12,7 +12,7 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-with open("laureates.csv", "r") as f:
+with open("laureates.csv", "r", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
 
